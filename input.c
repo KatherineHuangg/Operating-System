520112ca278208ac3e84e1e5414dff91ebf53a86
KatherineHuangg <61874435+KatherineHuangg@users.noreply.github.com>
2020-10-11 10:00:12 +0800

    Add files via upload

---
+++ input.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define var 50
+//5100000
+int main(){
+    int i,j,sign,number;
+    srand((unsigned int)time(NULL));
+    FILE *fout = fopen("input1.csv","w");
+    if(!fout){
+        printf("open fail\n");
+        exit(0);
+    }
+    for(i=0;i<var;i++){
+        for(j=0;j<20;j++){
+            sign = rand()%2 ==1 ? 1: -1;
+            if(sign == -1) number = sign * ((rand()+1)*(rand()+1)*(rand()%2 + 1));
+            else number = sign * (rand()+1) * (rand()+1) * (rand()%2 + 1) - 1;
+            if(j!=19) fprintf(fout,"%d|",number);
+            else fprintf(fout,"%d\n",number);
+        }
+    }
+
+    return 0;
+}
---
+++ transform.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+#define input_num 5100000
+
+int* apart(char* iput,int* num_arr){
+    int count = 0,i,index=1;
+    const char* delim = "||\r|\n";
+    char *saveptr = NULL;
+    char *number = NULL;
+    number = strtok(iput,delim);
+    printf("  {\n");
+    do{
+        if(index!=20) printf("    col_%d:%s,\n",index,number);
+        else printf("    col_%d:%s\n",index,number);
+        //printf("%s ",number);
+        number = strtok(NULL,delim);
+        index++;
+    }while(number);
+    printf("  },\n");
+}    
+
+void print(int* num_arr){  
+}
+
+
+int main(int argc,char* argv[]){
+    char* iput = (char*)malloc(sizeof(char)*240);
+    int* num_arr = (int*)malloc(sizeof(int)*20);
+    int i,index = 1;
+    int num_thread = atoi(argv[1]);
+    pthread_t tid[num_thread];
+
+    FILE *fin = fopen("input1.csv","r");
+    if(fin == NULL){
+        printf("fail to open\n");
+        exit(0);
+    }
+    while(fgets(iput,240,fin)!=NULL){
+        if(index!=num_thread) {
+            apart(iput,num_arr);//for thread
+            index++;
+        }
+        else if(index==num_thread){
+            apart(iput,num_arr);//for thread
+            index++;
+        }
+    }
+
+    fclose(fin);
+    free(iput);
+    free(num_arr);
+    return 0;
+}
