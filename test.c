1eeb7d8405584510e0ba77429956c1e3e185416e
KatherineHuangg <61874435+KatherineHuangg@users.noreply.github.com>
2020-10-14 21:05:31 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -9,7 +9,7 @@
 #include<sys/types.h>
 
 typedef struct{
-    int number;
+    int number;//第幾個thread
     int start;
     int end;
     char *str;
@@ -17,6 +17,29 @@ typedef struct{
     FILE *fput;
 }Thread;
 
+int *sum;//自己的thread要讀的行數(\n數量)
+
+void calculate(FILE *fp,int sum[],Thread thread[],int num_thread,int col[]){
+    char c;
+    char *string = (char*)malloc(sizeof(char)*240);
+    int i,have_read = 0,need_read = 0,length;
+
+    fseek(fp,0,SEEK_SET);
+    for(i=0;i<num_thread;i++){
+        //printf("%d ",thread[i].end);
+        need_read += thread[i].end+1;
+        while(fgets(string,240,fp)!=NULL){
+            length = strlen(string);
+            //printf("str:%d ",length);
+            have_read+=length;
+            sum[i]++;
+            if(have_read == need_read) break;
+        }
+        //printf("%d ",sum[i]);
+    }
+    //printf(" out cal\n");
+    free(string);
+}
 
 void assign(int num_thread,int amount,FILE *fp,int col[]){
     int i=0,j,move = amount; //move 表到下一個thread之間的長度;
@@ -29,19 +52,20 @@ void assign(int num_thread,int amount,FILE *fp,int col[]){
             get[0] = fgetc(fp);
             if(get[0]==EOF) {
                 col[i] = move;
-                printf("%d\n",move);
+                //printf("%d\n",col[i]);
                 break;
             }
             if((get[0] == '\n') && (j!=num_thread-1)){
                 move++;
                 col[i] = move;
                 i++;
-                printf("%d\n",move);
+                //printf("%d\n",col[i]);
                 break;
             }
             else move++;
         }
     }
+
 }
 
 void set(Thread thread[],int col[],int num_thread){
@@ -63,20 +87,47 @@ void set(Thread thread[],int col[],int num_thread){
 void* apart(void* arg){
     Thread *thr = (Thread*) arg;
     thr->str = (char*)malloc(sizeof(char)*240);
-    int index = thr->number;
+    int i,index=0;
+    //int index = thr->number;//是第幾個thread
     int position = thr->end;
 
+    const char* delim = "||\r|\n";
+    char *saveptr = NULL;
+    char *num = NULL;
+    int count = 1,length;
 
     fseek(thr->fin,thr->start,SEEK_SET);
-    while(position != ftell(thr->fin)){
-        printf("!%ld\n",ftell(thr->fin));
-        if(fgets(thr->str,240,thr->fin)!=NULL){
 
+    if(thr->number == 0) fseek(thr->fput,0,SEEK_SET);
+    else{
+        for(i=0;i<thr->number-1;i++) index+=sum[i];
+        printf("index:%d ",index);
+        fseek(thr->fput,279*sum[index]+thr->start,SEEK_SET);
+    }
+
+    while(position+1 != ftell(thr->fin)){
+        //printf("%d %d %d ",thr->start,thr->end,position+1);
+        //printf("%ld\n",ftell(thr->fin));
+        if(fgets(thr->str,240,thr->fin)!=NULL){
+            length = strlen(thr->str);
+            num = strtok_r(thr->str,delim,&saveptr);
+
+            
+            printf("sum:%d ",279*index+thr->start);
+            do{
+                if(count==1) fprintf(thr->fput,"  {\n    \"col_%d\":%s,\n",count,num);
+                else if(count!=20) fprintf(thr->fput,"    \"col_%d\":%s,\n",count,num);
+                else fprintf(thr->fput,"    \"col_%d\":%s\n",count,num);
+                num = strtok_r(NULL,delim,&saveptr);
+                count++;
+            }while(num);
+            count = 1;
+            fprintf(thr->fput,"  },\n");
         }
         else break;
-        printf("hi\n");
     }
-    printf("out\n");
+    
+    //printf("out\n");
     pthread_exit(0);
 }
 
@@ -117,6 +168,10 @@ int main(int argc,char* argv[]){
     ///set每個thread所需的資料
     set(thread,col,num_thread);
     
+    ///紀錄自己thread要讀的行數(\n數量)
+    sum = (int*)malloc(sizeof(int)*num_thread);
+    for(i=0;i<num_thread;i++) sum[i] = 0;
+    calculate(fp,sum,thread,num_thread,col);
 
     ///start
     for(i=0;i<num_thread;i++){
@@ -128,11 +183,15 @@ int main(int argc,char* argv[]){
         }
     }
     printf("before join\n");
-    //pthread_join(tid[num_thread],NULL);
-    //fclose(fin[num_thread]);
-
+    for(i=0;i<num_thread;i++){
+        pthread_join(tid[i],NULL);
+    }
+    
+    
+    //fprintf(fput,"\n]\n");
     //free(iput);
-    //pthread_exit(0);
+    free(sum);
+    pthread_exit(0);
     for(i=0;i<num_thread;i++){
         fclose(thread[i].fput);
         fclose(thread[i].fin);
